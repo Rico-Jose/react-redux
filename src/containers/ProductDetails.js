1783bb1171e4768d95400c5bdab04789f6e710c6
Rico <imbangjoserico@gmail.com>
2023-07-26 17:58:12 +0800

    Cleaned up the selected product when component is destroyed so product detail only shows the current and not the previous.

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectedProduct } from '../redux/actions/productActions';
+import {
+  selectedProduct,
+  removeSelectedProduct
+} from '../redux/actions/productActions';
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
@@ -22,6 +25,9 @@ const ProductDetails = () => {
 
   useEffect(() => {
     if (productId && productId !== "") fetchProductDetail();
+    return () => {
+      dispatch(removeSelectedProduct());
+    }
   }, [productId]);
 
   return (
